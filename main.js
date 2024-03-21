95328c577b52d8e27f2b78f62b47c74ef24be646
hashir ali <160639117+hashirali94@users.noreply.github.com>
2024-03-22 00:32:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var weightinkg = 70;
+var heightinmeters = 1.75;
+var bmi = weightinkg / (heightinmeters * heightinmeters);
+console.log("your bmi is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightinkg = 70;
+let heightinmeters = 1.75;
+let bmi = weightinkg / (heightinmeters * heightinmeters);
+console.log (`your bmi is ${bmi}`);
